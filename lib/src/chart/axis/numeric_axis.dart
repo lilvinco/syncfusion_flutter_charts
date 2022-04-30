2124c3766d6984f3cfe0a519602305c974e6ad72
lilvinco <victormba34@gmail.com>
2022-04-30 15:07:30 +0100

    tiy

--- lib/src/chart/axis/numeric_axis.dart
+++ lib/src/chart/axis/numeric_axis.dart
@@ -585,11 +585,11 @@ class NumericAxisDetails extends ChartAxisRendererDetails {
 
   /// Holds the value of axis padding
   // ignore:unused_field
-  final int axisPadding = 5;
+  final int axisPadding = 10;
 
   /// Holds the value of inner padding
   // ignore:unused_field
-  final int innerPadding = 5;
+  final int innerPadding = 25;
 
   @override
   late Size axisSize;
