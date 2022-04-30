bf5e777bc348486ec424739ff67e7a9e9810259f
lilvinco <victormba34@gmail.com>
2022-04-30 16:28:29 +0100

    series renderer

--- lib/src/chart/chart_series/series_renderer_properties.dart
+++ lib/src/chart/chart_series/series_renderer_properties.dart
@@ -294,14 +294,12 @@ class SeriesRendererDetails {
             series.markerSettings.width >= 0,
         'The width of the marker must be greater than or equal to 0.');
     for (int pointIndex = 0; pointIndex < dataPoints.length; pointIndex++) {
-      if (pointIndex == 0 || pointIndex == dataPoints.length - 1) {
-        final CartesianChartPoint<dynamic> point = dataPoints[pointIndex];
-        if ((series.markerSettings.isVisible &&
-                renderer is! BoxAndWhiskerSeriesRenderer) ||
-            renderer is ScatterSeriesRenderer) {
-          markerSettingsRenderer?.renderMarker(
-              this, point, animationController, canvas, pointIndex);
-        }
+      final CartesianChartPoint<dynamic> point = dataPoints[pointIndex];
+      if ((series.markerSettings.isVisible &&
+              renderer is! BoxAndWhiskerSeriesRenderer) ||
+          renderer is ScatterSeriesRenderer) {
+        markerSettingsRenderer?.renderMarker(
+            this, point, animationController, canvas, pointIndex);
       }
     }
   }
