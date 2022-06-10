a07a5930e3a80a75676449aa7ac6e4a57659ca64
lilvinco <victormba34@gmail.com>
2022-06-10 16:35:08 +0100

    pointtap show widget attempt

--- lib/src/common/utils/typedef.dart
+++ lib/src/common/utils/typedef.dart
@@ -51,7 +51,7 @@ typedef ChartZoomingCallback = void Function(ZoomPanArgs zoomingArgs);
 
 /// Returns the  ChartPointDetails.
 typedef ChartPointInteractionCallback = void Function(
-    ChartPointDetails pointInteractionDetails, dynamic position);
+    ChartPointDetails pointInteractionDetails, Offset position);
 
 /// Returns the  AxisLabelTapArgs.
 typedef ChartAxisLabelTapCallback = void Function(
